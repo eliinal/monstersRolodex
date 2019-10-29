20840cdda8ec6900162978eb093ee899dfc32197
Lundberg Elina <u054064@corp.saab.se>
2019-10-29 13:05:36 +0100

    Updates

---
+++ precache-manifest.ec64c9b0b51cf2bc7adcf554a9b8fc47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c04d2510486aa26f0fec80c0db8ab9",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "a70577fe50157ce4ae8a",
+    "url": "/monstersRolodex/static/css/main.12352d23.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monstersRolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "a70577fe50157ce4ae8a",
+    "url": "/monstersRolodex/static/js/main.f0c97988.chunk.js"
+  },
+  {
+    "revision": "e2cd670519ce94602d27",
+    "url": "/monstersRolodex/static/js/runtime-main.c197b2aa.js"
+  }
+]);
